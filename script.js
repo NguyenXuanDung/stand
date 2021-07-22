8c3573b22b7082ba8f206e2cc60c3f1e38fc2ad3
dxnguyen <dung.xuan.nguyen@mgm-tp.com>
2021-07-22 10:53:19 +0700

    add condition

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ function myTimer() {
   var minute = d.getMinutes();
   var second = d.getSeconds();
 
-  notifyMe();
-  if (minute == 5 && (second == 0 || second == 1)) {
-    console.log("DEN GIO");
+  if (minute == 0 && (second == 0 || second == 1)) {
+    notifyMe();
   }
 }
 
@@ -18,20 +17,16 @@ function notifyMe() {
     if (!window.Notification) {
         console.log('Browser does not support notifications.');
     } else {
-        // check if permission is already granted
         if (Notification.permission === 'granted') {
-            // show notification here
-            var notify = new Notification('Hi there!', {
-                body: 'Anh Dũng đây, đến giờ đứng dậy rồi, đừng ngồi nữa',
+            var notify = new Notification('Anh Dũng đây!', {
+                body: 'Đến giờ đứng dậy rồi, đừng ngồi nữa',
                 icon: 'https://scontent-hkg4-2.xx.fbcdn.net/v/t31.18172-8/26758295_1051581548357931_7003779812369033477_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=jt7qCESYLCwAX_16mkB&_nc_ht=scontent-hkg4-2.xx&oh=7dacc7b987fc21c2647afe2788f7da97&oe=611CC952',
             });
         } else {
-            // request permission from user
             Notification.requestPermission().then(function (p) {
                 if (p === 'granted') {
-                    // show notification here
-                    var notify = new Notification('Hi there!', {
-                        body: 'Anh Dũng đây, đến giờ đứng dậy rồi, đừng ngồi nữa',
+                    var notify = new Notification('Anh Dũng đây!', {
+                        body: 'Đến giờ đứng dậy rồi, đừng ngồi nữa',
                         icon: 'https://scontent-hkg4-2.xx.fbcdn.net/v/t31.18172-8/26758295_1051581548357931_7003779812369033477_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=jt7qCESYLCwAX_16mkB&_nc_ht=scontent-hkg4-2.xx&oh=7dacc7b987fc21c2647afe2788f7da97&oe=611CC952',
                     });
                 } else {
