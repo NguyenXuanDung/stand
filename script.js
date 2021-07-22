8b248311dca02dcd9d3fc89d567ca553fe54e298
dxnguyen <dung.xuan.nguyen@mgm-tp.com>
2021-07-22 11:14:22 +0700

    add go back desk message

--- script.js
+++ script.js
@@ -9,24 +9,27 @@ function myTimer() {
   var second = d.getSeconds();
 
   if (minute == 0 && (second == 0 || second == 1)) {
-    notifyMe();
+    notifyMe("Hãy đứng dậy vận động nào");
+  }
+  if (minute == 15 && (second == 0 || second == 1)) {
+    notifyMe("Đến giờ quay lại bàn rồi");
   }
 }
 
-function notifyMe() {
+function notifyMe(mes) {
     if (!window.Notification) {
         console.log('Browser does not support notifications.');
     } else {
         if (Notification.permission === 'granted') {
             var notify = new Notification('Anh Dũng đây!', {
-                body: 'Đến giờ đứng dậy rồi, đừng ngồi nữa',
+                body: mes,
                 icon: 'https://scontent-hkg4-2.xx.fbcdn.net/v/t31.18172-8/26758295_1051581548357931_7003779812369033477_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=jt7qCESYLCwAX_16mkB&_nc_ht=scontent-hkg4-2.xx&oh=7dacc7b987fc21c2647afe2788f7da97&oe=611CC952',
             });
         } else {
             Notification.requestPermission().then(function (p) {
                 if (p === 'granted') {
                     var notify = new Notification('Anh Dũng đây!', {
-                        body: 'Đến giờ đứng dậy rồi, đừng ngồi nữa',
+                        body: mes,
                         icon: 'https://scontent-hkg4-2.xx.fbcdn.net/v/t31.18172-8/26758295_1051581548357931_7003779812369033477_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=jt7qCESYLCwAX_16mkB&_nc_ht=scontent-hkg4-2.xx&oh=7dacc7b987fc21c2647afe2788f7da97&oe=611CC952',
                     });
                 } else {
