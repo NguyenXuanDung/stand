35f93fc46ea7ca0bded9a491ffd9de1e056f4208
dxnguyen <dung.xuan.nguyen@mgm-tp.com>
2021-07-22 10:22:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+  var d = new Date();
+  var t = d.toLocaleTimeString();
+  document.getElementById("demo").innerHTML = t;
+  var hour = d.getHours();
+  var minute = d.getMinutes();
+  var second = d.getSeconds();
+
+  notifyMe();
+  if (minute == 5 && (second == 0 || second == 1)) {
+    console.log("DEN GIO");
+  }
+}
+
+function notifyMe() {
+    if (!window.Notification) {
+        console.log('Browser does not support notifications.');
+    } else {
+        // check if permission is already granted
+        if (Notification.permission === 'granted') {
+            // show notification here
+            var notify = new Notification('Hi there!', {
+                body: 'How are you doing?',
+                icon: 'https://bit.ly/2DYqRrh',
+            });
+        } else {
+            // request permission from user
+            Notification.requestPermission().then(function (p) {
+                if (p === 'granted') {
+                    // show notification here
+                    var notify = new Notification('Hi there!', {
+                        body: 'How are you doing?',
+                        icon: 'https://bit.ly/2DYqRrh',
+                    });
+                } else {
+                    console.log('User blocked notifications.');
+                }
+            }).catch(function (err) {
+                console.error(err);
+            });
+        }
+    }
+}
